e5b15184453a6dfd7bae78335b51b74f4b2c049e
Satvik Chachra <50296407+satvikchachra@users.noreply.github.com>
2020-08-08 22:37:15 +0530

    Added Counter React App

---
+++ src/containers/Counter/Counter.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actionTypes from '../../store/actions';
+import CounterControl from '../../components/CounterControl/CounterControl';
+import CounterOutput from '../../components/CounterOutput/CounterOutput';
+
+class Counter extends Component {
+    state = {
+        counter: 0
+    }
+
+    counterChangedHandler = (action, value) => {
+        switch (action) {
+            case 'inc':
+                this.setState((prevState) => { return { counter: prevState.counter + 1 } })
+                break;
+            case 'dec':
+                this.setState((prevState) => { return { counter: prevState.counter - 1 } })
+                break;
+            case 'add':
+                this.setState((prevState) => { return { counter: prevState.counter + value } })
+                break;
+            case 'sub':
+                this.setState((prevState) => { return { counter: prevState.counter - value } })
+                break;
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={() => this.props.onIncrementCounter()} />
+                <CounterControl label="Decrement" clicked={() => this.props.onDecrementCounter()} />
+                <CounterControl label="Add 5" clicked={() => this.props.onAddCounter()} />
+                <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractCounter()} />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(storedResult =>
+                        <li
+                            key={storedResult.id}
+                            onClick={() => this.props.onDeleteResult(storedResult.id)}>
+                            {storedResult.value}
+                        </li>
+                    )}
+                </ul>
+            </div>
+        );
+    }
+}
+
+// Executed by React Redux Package
+
+// Parameter state is the state set up in store/reducer.js
+const mapStateToProps = state => {
+    return {
+        // prop names: values
+        ctr: state.counter,
+        storedResults: state.results
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+        onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
+        onAddCounter: () => dispatch({ type: actionTypes.ADD, val: 5 }),
+        onSubtractCounter: () => dispatch({ type: actionTypes.SUBTRACT, val: 5 }),
+        onStoreResult: () => dispatch({ type: actionTypes.STORE_RESULT }),
+        onDeleteResult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, resultElId: id })
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
