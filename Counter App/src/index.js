1c3741af312872e2402cb3c0d15efe740a57b258
Satvik Chachra <50296407+satvikchachra@users.noreply.github.com>
2020-08-15 21:20:37 +0530

    Adding Middleware

--- Counter App/src/index.js
+++ Counter App/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -15,7 +15,19 @@ const rootReducer = combineReducers({
   res: resultReducer
 });
 
-const store = createStore(rootReducer);
+// Name of middleware
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatching Action', action);
+      const result = next(action);
+      console.log('[Middleware] Next State', store.getState());
+      return result;
+    }
+  }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
